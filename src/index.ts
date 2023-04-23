78f9a583e3520369b464dd1d9a0a47a44931ee8d
Kai Walter <kai.walter@outlook.de>
2023-04-23 08:30:11 +0000

    bump up spin-js-sdk and fix Dapr startup behaviour

--- src/index.ts
+++ src/index.ts
@@ -10,31 +10,42 @@ const encoder = new TextEncoder();
 const decoder = new TextDecoder("utf-8");
 
 export const handleRequest: HandleRequest = async function (request: HttpRequest): Promise<HttpResponse> {
-  const orderId = "257"
 
-  // create a new order
-  const orders = [{
-    key: orderId.toString(),
-    value: { orderId: orderId }
-  }];
+  // do not process on Dapr startup health check
+  if(request.uri === "/hello") {
+    const orderId = "257"
 
-  const req = await fetch(`${stateStoreBaseUrl}?metadata.contentType=application/json`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(orders)
-    }
-  );
-    
-  const rec = await fetch(`${stateStoreBaseUrl}/${orderId}`);
-  const recBody = decoder.decode(await rec.arrayBuffer() || new Uint8Array());
+    // create a new order
+    const orders = [{
+      key: orderId.toString(),
+      value: { orderId: orderId }
+    }];
+
+    const req = await fetch(`${stateStoreBaseUrl}?metadata.contentType=application/json`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(orders)
+      }
+    );
+        
+    const rec = await fetch(`${stateStoreBaseUrl}/${orderId}`);
+    const recBody = decoder.decode(await rec.arrayBuffer() || new Uint8Array());
+
+    return {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+      body: encoder.encode(recBody).buffer
+    };
+  }
 
   return {
     status: 200,
-    headers: { "Content-Type": "application/json" },
-    body: encoder.encode(recBody).buffer
+    headers: {},
+    body: ""
   }
+
 }
 
